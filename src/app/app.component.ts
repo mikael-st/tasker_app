c5a7e8bd0dd8d5c63c97736c3d56dd47c527a935
mikael <mikael.st15@protonmail.com>
2024-11-30 17:54:20 -0300

    fix: error when clicking on the app bar completely and opening the profile fixed / now just click on the avatar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { LoginPage } from './screens/login-page/login-page.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
